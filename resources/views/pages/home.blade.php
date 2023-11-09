2dbb47b6716617d27e5628c29220642bd6abdda3
DESKTOP-01ILGOI\conta <contato@software-solution-it.com>
2023-11-09 00:24:30 -0300

    teste

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -250,6 +250,7 @@
                         <div class="hover-game-slide">
                             <form action="{{ route('playGame') }}" method="post">
                                 @csrf
+                                @method('POST')
                                 <input type="hidden" name="game_id" value="{{ $game['game_id'] }}">
                                 <button type="submit" class="play-game-slide">
                                     <svg focusable="false" aria-hidden="true" class="">
