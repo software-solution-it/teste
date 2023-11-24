e22c4f74e7588f4b85cedf365eae21b08a3089ba
DESKTOP-01ILGOI\conta <contato@software-solution-it.com>
2023-11-24 02:48:10 -0300

    teste

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -258,53 +258,52 @@
         </div>
 
         <div class="swiper-container swiper game-swiper2">
-    <div class="swiper-wrapper">
-            <div class="swiper-slide">
-            @foreach($jogos as $game)
-    <div class="swiper-slide">
-        <div class="game-slide">
-            @if(isset($game['local_image']) && is_string($game['local_image']))
-                <div class="img-game-slide" style="background-image: url({{ $game['local_image'] }});">
-                </div>
-            @else
-                <!-- Tratar caso $game['local_image'] não seja uma string -->
-            @endif
+            <div class="swiper-wrapper">
+                @foreach($jogos as $game)
+                    <div class="swiper-slide">
+                        <div class="game-slide">
+                            @if(isset($game['local_image']) && is_string($game['local_image']))
+                                <div class="img-game-slide" style="background-image: url({{ $game['local_image'] }});">
+                                </div>
+                            @else
+                                <!-- Tratar caso $game['local_image'] não seja uma string -->
+                            @endif
 
-            <div class="hover-game-slide">
-                <form action="{{ route('playGame', ['game_id' => $game['game_id'] ?? null]) }}" method="post">
-                    @csrf
-                    <button type="submit" class="play-game-slide">
-                        <svg focusable="false" aria-hidden="true" class="">
-                            <use xlink:href="/templates/default/img/betnew/svg-sprite.e1149d9.svg#icon-play"
-                                class="svg-use"></use>
-                        </svg>
-                    </button>
-                </form>
+                            <div class="hover-game-slide">
+                                <form action="{{ route('playGame', ['game_id' => $game['game_id'] ?? null]) }}" method="post">
+                                    @csrf
+                                    <button type="submit" class="play-game-slide">
+                                        <svg focusable="false" aria-hidden="true" class="">
+                                            <use xlink:href="/templates/default/img/betnew/svg-sprite.e1149d9.svg#icon-play"
+                                                class="svg-use"></use>
+                                        </svg>
+                                    </button>
+                                </form>
 
-                <div class="provider-game-slide">
-                    <a href="#">
-                        @if(isset($game['game_name']) && is_string($game['game_name']))
-                            {{ $game['game_name'] }}
-                        @else
-                            Nome do Jogo Não Disponível
-                        @endif
-                    </a>
-                </div>
-                <div class="provider-game-slide">
-                    Jogue agora!
-                </div>
+                                <div class="provider-game-slide">
+                                    <a href="#">
+                                        @if(isset($game['game_name']) && is_string($game['game_name']))
+                                            {{ $game['game_name'] }}
+                                        @else
+                                            Nome do Jogo Não Disponível
+                                        @endif
+                                    </a>
+                                </div>
+                                <div class="provider-game-slide">
+                                    Jogue agora!
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
+            <!-- Adicione as setas de navegação do Swiper -->
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
         </div>
     </div>
-@endforeach
-            </div>
-    </div>
-    <!-- Adicione as setas de navegação do Swiper -->
-    <div class="swiper-button-next"></div>
-    <div class="swiper-button-prev"></div>
 </div>
-    </div>
-</div>
+
 @if(Auth::user())
 <script>
     $('.banner-slot').click(function (e) {
