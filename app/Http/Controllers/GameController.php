984c6fbbe86982862b1e9dafd76936eb26be1ea9
DESKTOP-01ILGOI\conta <contato@software-solution-it.com>
2023-12-01 03:57:29 -0300

    apostas padroes

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -12,6 +12,9 @@ class GameController extends Controller
     public function __construct()
     {
         parent::__construct();
+        Log::info('GameController constructor called', [
+            'user' => $this->user,
+        ]);
         $this->redis = Redis::connection();
     }
 
