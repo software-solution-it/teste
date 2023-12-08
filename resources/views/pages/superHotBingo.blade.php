466656718dfa67d1501e84ddcd23a07402116f33
DESKTOP-01ILGOI\conta <contato@software-solution-it.com>
2023-12-08 01:20:47 -0300

    teste

--- resources/views/pages/superHotBingo.blade.php
+++ resources/views/pages/superHotBingo.blade.php
@@ -1,42 +1,30 @@
 @extends('layout')
 
 @section('content')
-<link rel="stylesheet" href="/css/mines.css">
-<link rel="stylesheet" href="/css/tower.css">
+    <link rel="stylesheet" href="/css/mines.css">
+    <link rel="stylesheet" href="/css/tower.css">
 
-<div class="section game-section">
-    <div class="container">
-        <div class="game">
-            <div class="game-component">
+    <div class="section game-section">
+        <div class="container">
+            <div class="game">
+                <div class="game-component">
+                    <div class="tower_tower__1ms3K">
+                        <iframe
+                            src="https://api-test.salsagator.com/game?token=teste2&pn=playbet-staging&lang=en&game=superHotBingo" style="width:100%; height:100%;"></iframe>
+                    </div>
 
-                <div class="tower_tower__1ms3K">
-                    <iframe
-                        src="https://api-test.salsagator.com/game?token=teste2&pn=playbet-staging&lang=en&game=superHotBingo"
-                        frameborder="0" style="width:100%; height:100%;"></iframe>
-                </div>
-                <div class="hits_Hits__1OApa">
-                    <div style="position: relative; height: 60px;" class="">
-                        <div class="xs hits_HitRow__2xXX"
-                            style="overflow-y: hidden; overflow-x: auto; position: absolute;">
+                    @guest
+                    <div class="game-sign">
+                        <div class="game-sign-wrap">
+                            <div class="game-sign-block auth-buttons">
+                                Você precisa estar logado para jogar
+                                <button type="button" class="btn" id="loginRegister">Entrar ou Cadastrar</button>
+                            </div>
                         </div>
                     </div>
+                    @endguest
                 </div>
-
-
-                @guest
-                <div class="game-sign">
-                    <div class="game-sign-wrap">
-                        <div class="game-sign-block auth-buttons">
-                            Você precisa estar logado para jogar
-                            <button type="button" class="btn" id="loginRegister">Entrar ou Cadastrar</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                @endguest
             </div>
-
         </div>
     </div>
-</div>
 @endsection
