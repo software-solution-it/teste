b21b0b65d25c5f12110589ca2001a098ffda1c6a
DESKTOP-01ILGOI\conta <contato@software-solution-it.com>
2023-12-09 03:28:44 -0300

    teste

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -358,7 +358,7 @@ class GameController extends Controller
                         </Returnset>
                     </Result>
                 </PKT>";
-                $user->update(['balance' => $user->balance - $params['BetAmount']['@attributes']['Value']]);
+                $user->update(['balance' => $user->balance - $params['BetAmount']['@attributes']['Value']] / 100);
             } else {
                 $response = "<PKT>
                     <Result Name='PlaceBet' Success='0'>
@@ -403,7 +403,7 @@ class GameController extends Controller
                         </Returnset>
                     </Result>
                 </PKT>";
-                $user->update(['balance' => $user->balance + $params['WinAmount']['@attributes']['Value']]);
+                $user->update(['balance' => $user->balance + $params['WinAmount']['@attributes']['Value']] / 100);
             } else {
                 $response = "<PKT>
                     <Result Name='AwardWinnings' Success='0'>
@@ -448,7 +448,7 @@ class GameController extends Controller
                         </Returnset>
                     </Result>
                 </PKT>";
-                $user->update(['balance' => $user->balance + $params['RefundAmount']['@attributes']['Value']]);
+                $user->update(['balance' => $user->balance + $params['RefundAmount']['@attributes']['Value']] / 100);
             } else {
                 $response = "<PKT>
                     <Result Name='RefundBet' Success='0'>
