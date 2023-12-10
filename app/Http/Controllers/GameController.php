cc02fd90c1649d6d5c535009dee5aa86c0fff7c0
DESKTOP-01ILGOI\conta <contato@software-solution-it.com>
2023-12-09 22:05:33 -0300

    remocao jogos

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -197,6 +197,11 @@ class GameController extends Controller
 
         $this->token = $params['Token']['@attributes']['Value'];
         $user = User::where('salsa_token', $this->token)->first();
+
+        Log::info('Response GetAccountDetails2', [
+            '$user->balance' => $user->balance,
+        ]);
+
         $user->balance = $user->balance * 100;
         $this->userLogged = trim($this->token);
 
@@ -333,7 +338,6 @@ class GameController extends Controller
 
     public function PlaceBet($params, $user){
         Log::info('Response GetAccountDetails2', [
-            '$user->balance' => $user->balance / 100,
             'BetAmount' => $params['BetAmount']['@attributes']['Value']
         ]);
 
