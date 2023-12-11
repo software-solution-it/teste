e954c6a32c44a63ee055767bc6cc52d17c28677c
DESKTOP-01ILGOI\conta <contato@software-solution-it.com>
2023-12-11 09:49:07 -0300

    teste

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -418,15 +418,22 @@ class GameController extends Controller
                     'TransactionID' => $params['TransactionID']['@attributes']['Value'],
                 ]);
 
+                if($user->transaction == null){
+                    $user->update(['transaction' =>$params['TransactionID']['@attributes']['Value']]); 
+                }
+
+                if($user->bet_reference_num == null){
+                    $user->update(['bet_reference_num' =>$params['BetReferenceNum']['@attributes']['Value']]);
+                }
+
                     if (isset($params['TransactionID']) && $params['TransactionID']['@attributes']['Value'] != null) {
                         if($params['TransactionID']['@attributes']['Value'] == $user->transaction
                             && $params['BETREFERENCE']['@attributes']['Value'] != $user->bet_reference_num){
-                            $resultValue = $user->balance;
+                            $resultValue = $user->balance - $params['BetAmount']['@attributes']['Value'];
                             $user->update(['bet_reference_num' =>$params['BetReferenceNum']['@attributes']['Value']]);
                         }else{
-                            $user->update(['transaction' =>$params['TransactionID']['@attributes']['Value']]); 
                             $user->update(['bet_reference_num' =>$params['BetReferenceNum']['@attributes']['Value']]);
-                            $resultValue = $user->balance - $params['BetAmount']['@attributes']['Value'];
+                            $resultValue = $user->balance;
                         }
                     }
   
